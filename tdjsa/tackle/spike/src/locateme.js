9225d8e4a9b704a698b230678c23eea6441dcc63
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2019-09-27 09:32:31 +0200

    Implement spike

--- tdjsa/tackle/spike/src/locateme.js
+++ tdjsa/tackle/spike/src/locateme.js
@@ -1,4 +1,4 @@
-var fetchLocation = function(onSuccess, onError) {
+const fetchLocation = function(onSuccess, onError) {
   var returnLocation = function(position) {
     var location = {
       lat: position.coords.latitude, lon: position.coords.longitude };
@@ -9,9 +9,15 @@ var fetchLocation = function(onSuccess, onError) {
   navigator.geolocation.getCurrentPosition(returnLocation, onError);
 };
 
+const gotoLocation = function(location) {
+  const url = "https://www.google.be/maps/@" + location.lat + "," + location.lon + "z"; //50.9471439,4.038005,13z
+  window.location = url;
+}
 
-var locate = function() {
-  this.fetchLocation(undefined, function(err) {
-    document.getElementById('error').innerHTML = "Something went wrong " + err.code;
-  });
+const showError = function(err) {
+  document.getElementById('error').innerHTML = "Something went wrong " + err.code;
+};
+
+const locate = function() {
+  fetchLocation(gotoLocation, showError);
 };
